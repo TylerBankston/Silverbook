3d622c69269806cd7bdd19a8c921a541410ad4b3
Tyler Bankston <tylerwbankston@gmail.com>
2016-06-08 21:18:17 -0600

    Leaving off for work tomorrow

---
+++ app/main.js
@@ -0,0 +1,14 @@
+﻿(function () {
+  'use strict';
+
+  angular
+    .module('silverbook')
+    .controller('searchController', searchController);
+
+  function searchController() {
+    var vm = this;
+
+    vm.query = '';
+  }
+    
+})();
